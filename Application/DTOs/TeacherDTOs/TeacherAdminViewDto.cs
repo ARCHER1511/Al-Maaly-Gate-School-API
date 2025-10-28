8cfb673e96b1c854f3197afa9df023380811794c
Ramy A.Mahana <ramymahana7@gmail.com>
2025-10-28 13:18:18 +0300

    Admin parts

---
+++ Application/DTOs/TeacherDTOs/TeacherAdminViewDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs.TeacherDTOs
+{
+    public class TeacherAdminViewDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ContactInfo { get; set; } = string.Empty;
+    }
+}
